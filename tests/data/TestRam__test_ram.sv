bd16713b28a9ca71280b6812d466fc32190214bb
Davide Libenzi <davide.libenzi@gmail.com>
2026-01-10 11:49:17 +0100

    Added RAM test.

---
+++ tests/data/TestRam__test_ram.sv
@@ -0,0 +1,49 @@
+/* verilator lint_off WIDTH */
+
+`timescale 1 ns / 100 ps
+
+
+package fp;
+  let MAX(A, B) = ((A > B) ? A : B);
+  let MIN(A, B) = ((A > B) ? B : A);
+  let ABS(A) = (($signed(A) >= 0) ? A : -$signed(A));
+  let FABS(A) = ((A >= 0.0) ? A : -A);
+
+  let EXP_OFFSET(NX) = (2**(NX - 1) - 1);
+endpackage
+
+// This in theory should be a typedef within the FPU interface, but then
+// many HDL tools do not support hierarchical type dereferencing.
+`define IEEE754(NX, NM) \
+struct packed { \
+  logic  sign; \
+  logic [NX - 1: 0] exp; \
+  logic [NM - 1: 0] mant; \
+  }
+
+
+// PyXHDL support functions.
+
+// Entity "RamTest" is "RamTest" with:
+// 	args={'CLK': 'bits(1)', 'RST_N': 'bits(1)', 'RDEN': 'bits(1)', 'WREN': 'bits(1)', 'ADDR': 'bits(12)', 'IN_DATA': 'bits(16)', 'OUT_DATA': 'bits(16)'}
+// 	kwargs={}
+module RamTest(CLK, RST_N, RDEN, WREN, ADDR, IN_DATA, OUT_DATA);
+  input logic CLK;
+  input logic RST_N;
+  input logic RDEN;
+  input logic WREN;
+  input logic [11: 0] ADDR;
+  input logic [15: 0] IN_DATA;
+  output reg [15: 0] OUT_DATA;
+  always @(posedge CLK)
+  run : begin
+    logic [15: 0] mem[4096];
+    if (&(!RST_N)) begin
+      OUT_DATA <= unsigned'(16'(0));
+    end else if (&RDEN) begin
+      OUT_DATA <= mem[int'(ADDR)];
+    end else if (&WREN) begin
+      mem[int'(ADDR)] <= IN_DATA;
+    end
+  end
+endmodule
---
+++ tests/t_ram.py
@@ -0,0 +1,43 @@
+import logging
+import unittest
+
+import py_misc_utils.utils as pyu
+
+import pyxhdl as X
+from pyxhdl import xlib as XL
+
+import test_utils as tu
+
+
+class RamTest(X.Entity):
+
+  PORTS = 'CLK, RST_N, RDEN, WREN, ADDR, IN_DATA, =OUT_DATA'
+
+  @X.hdl_process(sens='+CLK')
+  def run():
+    mem = X.mkreg(X.mkarray(X.UINT16, 2**ADDR.dtype.nbits))
+
+    if not RST_N:
+      OUT_DATA = 0
+    else:
+      if RDEN:
+        OUT_DATA = mem[ADDR]
+      elif WREN:
+        mem[ADDR] = IN_DATA
+
+
+class TestRam(unittest.TestCase):
+
+  def test_ram(self):
+    inputs = dict(
+      CLK=X.mkwire(X.BIT),
+      RST_N=X.mkwire(X.BIT),
+      RDEN=X.mkwire(X.BIT),
+      WREN=X.mkwire(X.BIT),
+      ADDR=X.mkwire(X.Bits(12)),
+      IN_DATA=X.mkwire(X.Bits(16)),
+      OUT_DATA=X.mkreg(X.Bits(16)),
+    )
+
+    tu.run(self, tu.test_name(self, pyu.fname()), RamTest, inputs)
+
