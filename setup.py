92b5356152f0061809b62ff83155993647f5a57f
Davide Libenzi <davide.libenzi@gmail.com>
2026-01-10 11:01:21 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyxhdl',
-      version='0.1',
+      version='0.2',
       description='Python HDL',
       author='Davide Libenzi',
       packages=find_packages(),
