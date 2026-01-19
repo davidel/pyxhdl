b0c9e5a867b2c9aff10891ce634fe15e2000d7ed
Davide Libenzi <davide.libenzi@gmail.com>
2026-01-19 18:05:39 +0100

    Bump version and requires recent Python.

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyxhdl',
-      version='0.2',
+      version='0.25',
       description='Python HDL',
       author='Davide Libenzi',
       packages=find_packages(),
       package_data={
           'pyxhdl': ['hdl_libs/**', ],
       },
+      python_requires='>=3.5',
       install_requires=[
           'numpy',
           'py_misc_utils @ git+https://github.com/davidel/py_misc_utils@main',
