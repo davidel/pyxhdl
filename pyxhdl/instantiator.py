892bfac2a5e4a5f36c7734b6e41e8da7426d8f60
Davide Libenzi <davide.libenzi@gmail.com>
2026-02-06 15:08:33 +0100

    Allow Ability to force new revisions.

--- pyxhdl/instantiator.py
+++ pyxhdl/instantiator.py
@@ -27,14 +27,14 @@ class Instanciator:
     self._revgen = pycu.RevGen(revbase=revbase if revbase is not None else 1)
 
   def _cname(self, name):
-    cname = re.sub(r'[.$:]+', '_', name)
+    cname = re.sub(r'\W+', '_', name)
 
     return cname
 
-  def getid(self, name, kwargs):
+  def getid(self, name, kwargs, force_new=False):
     params = kwargs.pop(self._param_key, dict()) if self._param_key else dict()
     inst = _Instance(name, params, kwargs)
-    iid = self._instances.get(inst)
+    iid = self._instances.get(inst) if not force_new else None
     if iid is None:
       cname = self._cname(name)
       iid = self._revgen.newname(cname)
