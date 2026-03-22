94cbf82a17640ee1de2b9c0f612081f737aa3725
Davide Libenzi <davide.libenzi@gmail.com>
2026-03-22 08:21:33 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyxhdl',
-      version='0.43',
+      version='0.44',
       description='Use Python as HDL language, and generate equivalent SystemVerilog and VHDL code',
       author='Davide Libenzi',
       packages=find_packages(),
