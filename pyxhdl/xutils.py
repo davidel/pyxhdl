9f1f79f15574238949a0d28680be7c0d2df766f8
Davide Libenzi <davide.libenzi@gmail.com>
2026-05-04 16:09:46 +0200

    Drop extra line.

--- pyxhdl/xutils.py
+++ pyxhdl/xutils.py
@@ -67,7 +67,6 @@ def split(src, *sizes, base=0) -> tuple[Value]:
   return tuple(values)
 
 
-
 def bitfill(bitval, n):
   bitstr = str(bitval) if not isinstance(bitval, str) else bitval
   if bitstr not in VALID_BITS:
